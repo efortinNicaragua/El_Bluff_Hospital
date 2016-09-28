7fd2f503c1ce703e96861a7b6668ceac01e5b926
Chris Dixon <cdixon8@villanova.edu>
2016-09-28 01:53:01 -0400

    Updated Screen:   - SeeInventory
     Added Blank Screens:
      SeeInventory > SearchTotals
      SeeInventory > TotalInventory
      SeeInventory > SearchShipments

---
+++ app/src/main/java/ethanfortin_nicaragua/elbluffhospital/SearchShipments.java
@@ -0,0 +1,13 @@
+package ethanfortin_nicaragua.elbluffhospital;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchShipments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_shipments);
+    }
+}
--- app/src/main/java/ethanfortin_nicaragua/elbluffhospital/SeeInventory.java
+++ app/src/main/java/ethanfortin_nicaragua/elbluffhospital/SeeInventory.java
@@ -1,7 +1,9 @@
 package ethanfortin_nicaragua.elbluffhospital;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SeeInventory extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class SeeInventory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_see_inventory);
+    }
+
+    public void searchTotals(View v) {
+        Intent searchTots = new Intent(this, SearchTotals.class);
+        startActivity(searchTots);
+    }
+
+    public void searchShipments(View v) {
+        Intent searchShips = new Intent(this, SearchShipments.class);
+        startActivity(searchShips);
+    }
+
+    public void entireInventory(View v) {
+        Intent entInv = new Intent(this, Inventory.class);
+        startActivity(entInv);
     }
 }
