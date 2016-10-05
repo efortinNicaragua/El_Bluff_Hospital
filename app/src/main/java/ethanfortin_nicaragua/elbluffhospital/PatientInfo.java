7a03feedcce6ed9224463cdece7db1591ca373b2
efortinNicaragua <efortin@villanova.edu>
2016-10-05 10:28:10 -0400

    Added Rules for Buscar Button for Patient Look Up Screen

--- app/src/main/java/ethanfortin_nicaragua/elbluffhospital/PatientInfo.java
+++ app/src/main/java/ethanfortin_nicaragua/elbluffhospital/PatientInfo.java
@@ -1,13 +1,85 @@
 package ethanfortin_nicaragua.elbluffhospital;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class PatientInfo extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_info);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public void Buscar (View v) {
+        EditText name_EditText = (EditText) findViewById(R.id.edit_name);
+        EditText id_EditText = (EditText) findViewById(R.id.edit_ID);
+        String sName = name_EditText.getText().toString();
+        String sID = id_EditText.getText().toString();
+        if ((sName.matches("")) & (sID.matches("")))
+        {
+            Toast.makeText(this, "Necesitas Entrar un ID o un Nombre", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            Intent intent_phmain = new Intent(this, PharmacyMain.class);
+            startActivity(intent_phmain);
+        }
+    }
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("PatientInfo Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
