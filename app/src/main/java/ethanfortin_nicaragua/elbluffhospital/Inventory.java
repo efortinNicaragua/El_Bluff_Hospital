40c143243e20dfe2afe6eacbe90c4e17a3bba7f0
efortinNicaragua <efortin@villanova.edu>
2016-12-05 20:38:33 -0500

    Three Screens "finished" Still have updates and cleaning up to do, still need to do app versions of files, still have to fix dialog changing text on histroy and general info

--- app/src/main/java/ethanfortin_nicaragua/elbluffhospital/Inventory.java
+++ app/src/main/java/ethanfortin_nicaragua/elbluffhospital/Inventory.java
@@ -3,108 +3,205 @@ package ethanfortin_nicaragua.elbluffhospital;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 import static android.R.id.list;
-//import static ethanfortin_nicaragua.elbluffhospitalan.android.R.id.listView_fetch_druginfo_all;
 
-public class Inventory extends ListActivity{//implements interface_DruginfoAll {
-//Create global variables for view
+public class Inventory extends ListActivity {
 
-    Context context;
+//Create global variables for list view and ArrayList<Class_FetchAllDrugInfo>
     ListView listView;
-    String Drop;
-     //fetch_druginfo_all asyncTask  = new fetch_druginfo_all(context);
-     ArrayList<Class_FetchAllDrugInfo> druginfo_data = new ArrayList();
+    ArrayList<Class_FetchAllDrugInfo> druginfo_data = new ArrayList();
 
+//Standard onCreate methos
     @Override
     public void onCreate(Bundle savedInstanceState) {
-       // asyncTask.delegate = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
 
-    ListView listview2= (ListView) findViewById(android.R.id.list);
-    }
-
-    public void jumpTo(View v) {
-        String link = "http://153.104.62.31//android_connect/fetch_druginfo_all.php";///android_connect
-       /*try {
-            URL url = new URL(link);
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            AlertDialog alertDialog;
-            alertDialog= new AlertDialog.Builder(this).create();
-            alertDialog.setTitle("Connection");
-            alertDialog.setMessage("IT worked ");
-            alertDialog.show();
-        }
-        catch (Exception ex){
-        AlertDialog alertDialog1;
-        alertDialog1= new AlertDialog.Builder(this).create();
-        alertDialog1.setTitle("Connection");
-        alertDialog1.setMessage("Could not connect ya dingus, bad url ");
-        alertDialog1.show();}*/
+        //For presentation
 
 
-        fetch_druginfo_all fetch_druginfo_all=new fetch_druginfo_all(this);
-         fetch_druginfo_all.execute();
-
-       // asyncTask.execute();
+        fetch_druginfo_all fetch_druginfo_all = new fetch_druginfo_all(this);
+        fetch_druginfo_all.execute();
 
 
     }
 
-    public void setArrayList( ArrayList<Class_FetchAllDrugInfo> ArrayList) {
-        //Drop="";
-        //this.Drop = go;
-        this.druginfo_data = ArrayList;
+
+    public class fetch_druginfo_all extends AsyncTask<Void, Class_FetchAllDrugInfo, String> {
+
+        Context context;
+        AlertDialog alertDialog;
+        String json_string;
+        JSONObject jsonObject;
+        private Inventory activity;
+        private View rootView;
+        JSONArray jsonArray;
+        LayoutInflater inflater;
+        ArrayList<Class_FetchAllDrugInfo> druginfo_data = new ArrayList();
+
+        public fetch_druginfo_all(Context ctx) {
+            context=ctx;
 
         }
 
-    public void loadTo(View v)
-    {
-       /* int i =123;
-        String j="123452";
-        String p="Tylenol";
-        druginfo_data.add(new Class_FetchAllDrugInfo(j,p,i));
-        druginfo_data.add(new Class_FetchAllDrugInfo("12345", "Dugs", 1234))*/
-        ArrayAdapter<Class_FetchAllDrugInfo> adapter= new ArrayAdapter_FetchAllDrugInfo(this,druginfo_data);
+        @Override
+        protected String doInBackground(Void... parms) {
+            String link = "http://192.168.0.100/android_connect/fetch_druginfo_all.php"; //153.104
+            String user_name = "Ethan";
+            String password = "GoCatz";
+            String data;
+            String fail = "fail";
+            try {
+                //Create and open the URL connection
+                URL url = new URL(link);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                //setDoOutput allows you to access PHP server?
+                //setDoInput lets you to recieve data from PHP server?
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                //OutputStream Writer is how you write the data along with the connection request
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                //BufferedWriter lets you write to the PHP clasuse
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                //This is how you send JSON values along with the request
+                String post_data = URLEncoder.encode("user_name", "UTF-8") + "=" + URLEncoder.encode(user_name, "UTF-8") + "&"
+                        + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
 
+                //bufferedWriter writes data
+                bufferedWriter.write(post_data);
+                //Deletes data, closes everything
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
 
+                //we will store result in string builder
+                StringBuilder sb = new StringBuilder();
+                String result = "";
+                //Input from PHP
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                //Writting data from JSON in to result(string) and sb(String Builder)
+                String line = "";
+                while ((line = bufferedReader.readLine()) != null) {
+                    result += line;
+                    sb.append(line);
+                }
+                //Closing all things we opened
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
 
-        for(int crtx=0; crtx<druginfo_data.size();crtx++)
-        {
-            Class_FetchAllDrugInfo drug1;
-            drug1= druginfo_data.get(crtx);
-            System.out.println(drug1.getAllData());
+                //Turning StringBuilder to JSONObject
+                //JSONObject tempJsonObj = new JSONObject(sb.toString());
+
+                //Manipulating JSON DATA into something useful
+                //JSONObject mainObj=new JSONObject(tempJsonOb;
+
+                json_string = sb.toString().trim();
+
+            } catch (MalformedURLException e) {
+               //need to add exception handler
+
+            } catch (IOException e) {
+
+                //need to add exception handler
+
+            }
+            return json_string;
         }
-        ListView listView=(ListView) findViewById(android.R.id.list);
-        listView.setAdapter(adapter);
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        //doInBackground return value goes here to onPostExecute
+        @Override
+        protected void onPostExecute(String json_string) {
+        //we try to convert string to jsonObject
+            try {
+                //listView = (ListView) findViewById(R.id.activity_add_medicine);
+
+                jsonObject = new JSONObject(json_string);
+                //from object we make json array druginfo because that is what it is called in php
+                jsonArray = jsonObject.getJSONArray("druginfo");
+
+
+                //initiliaze count for while loop, strings for all data we will get from json
+                //all data comes out as strings so for int values we need to cast them into int values later
+                //hence drugtotal has int and string variables
+                int count = 0;
+                String drugid, drugname, drugtotal;
+                int drugtotal_int;
+
+                //while count is less than length of jsonarray
+                while (count < jsonArray.length()) {
+                    //get the object put drugid into drugid ect..
+                    JSONObject JO = jsonArray.getJSONObject(count);
+                    drugid = JO.getString("drugid");
+                    drugname = JO.getString("drugname");
+                    drugtotal = JO.getString("drugtotal");
+
+                    //try to cast string into int
+                    try {
+                        drugtotal_int = Integer.parseInt(drugtotal);
+                    //add this data as Class_FetchAllDrugInfo to ArrayList
+                        druginfo_data.add(new Class_FetchAllDrugInfo(drugid, drugname, drugtotal_int));
+
+                    } catch (NumberFormatException nfe) {
+                        //make exception handlers?
+                    }
+                    //increment count
+                    count++;
+                }
+
+            } catch (JSONException e) {
+                //make excetion handler
+
+            }
+
+
+
+            //Initiliaze arrayadapter and provide it with context and arraylist
+            ArrayAdapter<Class_FetchAllDrugInfo> adapter = new ArrayAdapter_FetchAllDrugInfo(context, druginfo_data);
+
+            //setliest view to lsitview in the xml file
+            ListView listView=(ListView) findViewById(android.R.id.list);
+            //turn on list view
+            listView.setAdapter(adapter);
+
+        }
+
     }
-    /*@Override
-    public void processFinish(Class_FetchAllDrugInfo[] output) {
-
-
-        ArrayAdapter_FetchAllDrugInfo adapter = new ArrayAdapter_FetchAllDrugInfo(context,
-                R.layout.row_druginventory_all, output);
-        listView= (ListView) listView.findViewById(listView_fetch_druginfo_all);
-        listView.setAdapter(adapter);
-
-
-    }*/
 }
 
