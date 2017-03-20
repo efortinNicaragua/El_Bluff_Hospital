14abeffb587c93aa4d2ab2cbbc306d0892e3f5fe
Chris Dixon <cdixon8@villanova.edu>
2017-03-20 15:05:13 -0400

    Small name changes - SHIPMENTS -> SHIPMENT

--- app/src/main/java/ethanfortin_nicaragua/elbluffhospital/ConnVars.java
+++ app/src/main/java/ethanfortin_nicaragua/elbluffhospital/ConnVars.java
@@ -10,7 +10,9 @@ public class ConnVars {
     // IP Addresses for PHP Scripts
     public static final String URL_FETCH_SPECIFIC_DRUG = "http://192.168.0.100/android_connect/fetch_druginfo_row.php";
     public static final String URL_FETCH_DRUGINFO_ALL = "http://192.168.0.100/android_connect/fetch_druginfo_all.php";
-    public static final String URL_FETCH_SHIPMENTS = "http://192.168.0.100/android_connect/fetch_shipment_rows.php";
+    public static final String URL_FETCH_SHIPMENT = "http://192.168.0.100/android_connect/fetch_shipment_row.php";
+
+    public static final String URL_ADD_SHIPMENT = "http://192.168.0.100/android_connect/add_shipment_row.php";
 
     // JSON Tags
 
@@ -21,10 +23,11 @@ public class ConnVars {
     public static final String TAG_DRUGINFO_NAME = "drugname";
     public static final String TAG_DRUGINFO_QUANT = "drugtotal";
 
-    public static final String TAG_SHIPMENTS = "shipment";
-    public static final String TAG_SHIPMENTS_SHIPDATE = "shipdate";
-    public static final String TAG_SHIPMENTS_DRUGID = "drugid";
-    public static final String TAG_SHIPMENTS_DRUGNAME = "drugname";
-    public static final String TAG_SHIPMENTS_SHIPQUANT = "shipquant";
+    public static final String TAG_SHIPMENT = "shipment";
+    public static final String TAG_SHIPMENT_SHIPDATE = "shipdate";
+    public static final String TAG_SHIPMENT_DRUGID = "drugid";
+    public static final String TAG_SHIPMENT_DRUGNAME = "drugname";
+    public static final String TAG_SHIPMENT_SHIPQUANT = "shipquant";
+    public static final String TAG_SHIPMENT_DRUGINCR = "drugincr";
 
 }
