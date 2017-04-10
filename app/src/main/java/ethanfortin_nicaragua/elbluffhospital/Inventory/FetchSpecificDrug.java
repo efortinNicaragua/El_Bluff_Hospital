2fd452c69a13e1c9794d3cb56735b43e99f24b58
Chris Dixon <cdixon8@villanova.edu>
2017-04-10 17:23:46 -0400

    Updated Fetch_Specific_Drug with a filterable listview

--- app/src/main/java/ethanfortin_nicaragua/elbluffhospital/Inventory/FetchSpecificDrug.java
+++ app/src/main/java/ethanfortin_nicaragua/elbluffhospital/Inventory/FetchSpecificDrug.java
@@ -5,41 +5,45 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import ethanfortin_nicaragua.elbluffhospital.ArrayAdapters.DrugNameAdapter;
 import ethanfortin_nicaragua.elbluffhospital.ArrayAdapters.DruginfoAdapter;
+import ethanfortin_nicaragua.elbluffhospital.ArrayAdapters.PatientinfoAdapter;
 import ethanfortin_nicaragua.elbluffhospital.ConnVars;
 import ethanfortin_nicaragua.elbluffhospital.DataClasses.DruginfoFields;
 import ethanfortin_nicaragua.elbluffhospital.R;
 import ethanfortin_nicaragua.elbluffhospital.RequestHandler;
+import ethanfortin_nicaragua.elbluffhospital.SpinnerWidget.SearchableSpinner;
 
-public class FetchSpecificDrug extends AppCompatActivity implements View.OnClickListener {
+public class FetchSpecificDrug extends AppCompatActivity implements SearchableSpinner.OnSelectionChangeListener {
 
     // Declare global layout variables
-
-    // search parameters
-    private EditText editTextDrugName;
-    private EditText editTextDrugId;
-
-    // button listener
-    private Button buttonSearch;
+    ArrayAdapter<String> adapter;
 
     // druginfo class array adapter
     ArrayList<DruginfoFields> druginfo = new ArrayList();
 
+    ListView lv;
+    String selectedListItem;
 
     // Set layout, initialize layout object handles and listener
     @Override
@@ -47,17 +51,101 @@ public class FetchSpecificDrug extends AppCompatActivity implements View.OnClick
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fetch_specific_drug);
 
-        buttonSearch = (Button) findViewById(R.id.bFind);
+        nameListFetch();
 
-        editTextDrugName = (EditText) findViewById(R.id.etDrugName);
-        editTextDrugId = (EditText) findViewById(R.id.etDrugID);
-
-        buttonSearch.setOnClickListener(this);
     }
 
-    // argChoice = 1 --> search by name
-    // argChoice = 2 --> search by ID
-    private void drugFetch(final String name, final String id) {
+    // Fetches JSON String of all drugs in inventory
+    private void nameListFetch() {
+
+        class fetchNameList extends AsyncTask<Void,Void,String> {
+
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(FetchSpecificDrug.this,"Buscando...","Espera, por favor",false,false);
+            }
+
+            // Once JSON received correctly, parse and display it
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+
+                String drugName;
+                lv = (ListView)findViewById(R.id.drug_list);
+                ArrayList<String> list = new ArrayList<>();
+                int count = 0;
+
+                try {
+                    JSONObject jsonObject = new JSONObject(s);
+                    JSONArray resArr = jsonObject.getJSONArray(ConnVars.TAG_DRUGINFO);
+
+                    while (count < resArr.length()) {
+                        System.out.println("Array: " + list);
+                        JSONObject resObj = resArr.getJSONObject(count);
+                        drugName = resObj.getString(ConnVars.TAG_DRUGINFO_NAME);
+                        list.add(drugName);
+                        count++;
+                    }
+
+                } catch (JSONException j) {
+                    System.out.println("JSON Exception occurred...");
+                }
+
+                adapter = new DrugNameAdapter(FetchSpecificDrug.this, list);
+
+                final EditText filter = (EditText)findViewById(R.id.filter_bar);
+                lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                        selectedListItem = (String) lv.getItemAtPosition(position);
+                        System.out.println("ITEM = " + selectedListItem);
+                        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                        mgr.hideSoftInputFromWindow(filter.getWindowToken(), 0);
+                        drugFetch(selectedListItem);
+                    }
+                });
+
+                lv.setAdapter(adapter);
+
+
+                filter.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        FetchSpecificDrug.this.adapter.getFilter().filter(s);
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+
+                    }
+                });
+            }
+
+            // Pass user input params to fetch method, even if they are blank
+            protected String doInBackground(Void... params) {
+
+                String s;
+                RequestHandler reqHan = new RequestHandler();
+                s = reqHan.sendGetRequest(ConnVars.URL_FETCH_DRUGINFO_ALL);
+
+                return s;
+            }
+        }
+
+        fetchNameList nameList = new fetchNameList();
+        nameList.execute();
+    }
+
+    // Takes entered name and Id as args,
+    private void drugFetch(final String name) {
 
         class fetchSpecificDrug extends AsyncTask<Void,Void,String> {
 
@@ -78,15 +166,15 @@ public class FetchSpecificDrug extends AppCompatActivity implements View.OnClick
                 drugShow(s);
             }
 
-            // In here, split between argChoice Value (1 or 2)
+            // Pass user input params to fetch method, even if they are blank
             protected String doInBackground(Void... params) {
 
                 RequestHandler reqHan = new RequestHandler();
                 HashMap<String, String> map = new HashMap<>();
                 String s;
 
+                // Will only pass key-value if EditText was populated
                 if (!TextUtils.equals("", name)) map.put("drugname", name);
-                if (!TextUtils.equals("", id)) map.put("drugid", id);
 
                 s = reqHan.sendGetRequestParam(ConnVars.URL_FETCH_SPECIFIC_DRUG, map);
 
@@ -102,6 +190,11 @@ public class FetchSpecificDrug extends AppCompatActivity implements View.OnClick
     // Parses JSON - listview involved
     private void drugShow(String json) {
 
+        TextView txtName = (TextView)findViewById(R.id.name_res);
+        TextView txtId = (TextView)findViewById(R.id.id_res);
+        TextView txtQuant = (TextView)findViewById(R.id.quant_res);
+        System.out.println(json);
+
         try {
             System.out.println("Mark 1");
             JSONObject jsonObject = new JSONObject(json);
@@ -112,42 +205,17 @@ public class FetchSpecificDrug extends AppCompatActivity implements View.OnClick
             String drugId = resObj.getString(ConnVars.TAG_DRUGINFO_ID);
             String drugTotal = resObj.getString(ConnVars.TAG_DRUGINFO_QUANT);
 
-            druginfo.add(new DruginfoFields(
-                    drugName,
-                    drugId,
-                    Integer.parseInt(drugTotal)
-            ));
+            txtName.setText(drugName);
+            txtId.setText(drugId);
+            txtQuant.setText(drugTotal);
 
         } catch (JSONException j) {
             System.out.println("JSON Exception occurred...");
         }
-
-        Context context = this;
-        ListView listView;
-        ArrayAdapter<DruginfoFields> adapter = new DruginfoAdapter(context, druginfo);
-
-        // Sync ListView object with widget in layout file
-        listView = (ListView) findViewById(R.id.list);
-        listView.setAdapter(adapter);
     }
 
     @Override
-    public void onClick(View v) {
-
-        // Get
-        String sName = editTextDrugName.getText().toString();
-        String sId = editTextDrugId.getText().toString();
-
-        // If neither field is populated, give toast
-        if (TextUtils.equals("", sName) && TextUtils.equals("", sId)) {
-
-            int duration = Toast.LENGTH_LONG;
-            Context context = getApplicationContext();
-            String text1 = "Da el nombre o el ID de la medicina, por favor";
-            Toast toast1 = Toast.makeText(context, text1, duration);
-            toast1.show();
-        }
-        // Else, one or both fields are populated
-        else drugFetch(sName, sId);
+    public void onSelectionChanged(String selection) {
+        Toast.makeText(this, selection + " selected", Toast.LENGTH_SHORT).show();
     }
 }
